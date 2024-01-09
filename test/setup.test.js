1d0f8ae79fff16c785a7f69245f4edbaf0e562dd
Zachary Brown <z.brown@chia.net>
2024-01-09 14:33:57 -0800

    test: integration tests in ci

--- test/setup.test.js
+++ test/setup.test.js
@@ -1,6 +1,7 @@
 const superagent = require("superagent");
 
-// Hosts should have the full path including port and any subdirectories
+// Hosts should have the full path including scheme (http or https), port, and any subdirectories.
+// No trailing slash.
 const { CADT_HOST } = process.env
 const { EXPLORER_HOST } = process.env
 const { TOKENIZATION_ENGINE_HOST } = process.env
@@ -9,7 +10,7 @@ const { TOKENIZATION_ENGINE_HOST } = process.env
 describe("Uptime Tests", () => {
   it("CADT should pass", async () => {
     const response = await superagent.get(
-      `http://${CADT_HOST}/health`
+      `${CADT_HOST}/health`
     );
 
     expect(response.status).toBe(200);
@@ -17,7 +18,7 @@ describe("Uptime Tests", () => {
 
   it("Climate Explorer should pass", async () => {
     const response = await superagent.get(
-      `http://${EXPLORER_HOST}/v1/activities`
+      `${EXPLORER_HOST}/v1/activities`
     );
 
     expect(response.status).toBe(200);
@@ -25,7 +26,7 @@ describe("Uptime Tests", () => {
 
   it("Climate Tokeninazation Engine should pass", async () => {
     const response = await superagent.get(
-      `http://${TOKENIZATION_ENGINE_HOST}/healthz`
+      `${TOKENIZATION_ENGINE_HOST}/healthz`
     );
 
     expect(response.status).toBe(200);

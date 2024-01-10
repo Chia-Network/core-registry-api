3d06c4811e72ef5b1c218603f87aca0a43239ecd
Zachary Brown <z.brown@chia.net>
2024-01-09 16:54:11 -0800

    test: fix api_key syntax

--- test/setup.test.js
+++ test/setup.test.js
@@ -10,27 +10,24 @@ const { API_KEY } = process.env
 describe("Uptime Tests", () => {
   it("CADT should pass", async () => {
     const response = await superagent
-      .get(`${CADT_HOST}/health`
-      .set("x-api-key",${API_KEY})
-    );
+      .get(`${CADT_HOST}/health`)
+      .set("x-api-key",API_KEY);
 
     expect(response.status).toBe(200);
   });
 
   it("Climate Explorer should pass", async () => {
     const response = await superagent
-    .get(`${EXPLORER_HOST}/v1/activities`
-    .set("x-api-key",${API_KEY})
-    );
+    .get(`${EXPLORER_HOST}/v1/activities`)
+    .set("x-api-key",API_KEY);
 
     expect(response.status).toBe(200);
   });
 
   it("Climate Tokeninazation Engine should pass", async () => {
     const response = await superagent
-    .get(`${TOKENIZATION_ENGINE_HOST}/healthz`
-    .set("x-api-key",${API_KEY})
-    );
+    .get(`${TOKENIZATION_ENGINE_HOST}/v1/healthz`)
+    .set("x-api-key",API_KEY);
 
     expect(response.status).toBe(200);
   });

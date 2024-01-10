ffd4fad2e137a1616e271b18abb39ed3a3aa0ea1
Zachary Brown <z.brown@chia.net>
2024-01-09 16:19:36 -0800

    test: add api key variable

--- test/setup.test.js
+++ test/setup.test.js
@@ -5,28 +5,31 @@ const superagent = require("superagent");
 const { CADT_HOST } = process.env
 const { EXPLORER_HOST } = process.env
 const { TOKENIZATION_ENGINE_HOST } = process.env
-
+const { API_KEY } = process.env
 
 describe("Uptime Tests", () => {
   it("CADT should pass", async () => {
-    const response = await superagent.get(
-      `${CADT_HOST}/health`
+    const response = await superagent
+      .get(`${CADT_HOST}/health`
+      .set("x-api-key",${API_KEY})
     );
 
     expect(response.status).toBe(200);
   });
 
   it("Climate Explorer should pass", async () => {
-    const response = await superagent.get(
-      `${EXPLORER_HOST}/v1/activities`
+    const response = await superagent
+    .get(`${EXPLORER_HOST}/v1/activities`
+    .set("x-api-key",${API_KEY})
     );
 
     expect(response.status).toBe(200);
   });
 
   it("Climate Tokeninazation Engine should pass", async () => {
-    const response = await superagent.get(
-      `${TOKENIZATION_ENGINE_HOST}/healthz`
+    const response = await superagent
+    .get(`${TOKENIZATION_ENGINE_HOST}/healthz`
+    .set("x-api-key",${API_KEY})
     );
 
     expect(response.status).toBe(200);
